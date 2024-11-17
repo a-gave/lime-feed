fe5300a41e83fc40e09c6e9b1e31048490016666
a-gave <a-gave@users.noreply.github.com>
2024-11-17 19:29:25 +0000

    deploy: a-gave/lime-packages@e270a56d9cf7d1a1056e960eb9e1ff710d13f162

---
+++ arch_packages/master/openwrt_main/arm_cortex-a7_vfpv4/packages.adb
@@ -0,0 +1,6 @@
+ADBd}TmhEÞÜ®riÚziO[KÑ)\mée/÷•í55G´6VL•ühŒs»sw“ìí¬»sŸh<~¢*˜€EÛ+4P?‚BSiU‚ª-B~FhUJÕŠšÐw÷®1QpááÝ÷Ù™ygžçíìÚ ºZøQœgVæê4š!²U´8ÉÜ†£P(jµDB­Í¡æ¦°÷«ŸÆ>¾þØåmƒõ“RÄÛó¨û5´[TA•y(I5b¡$3Ñ^ÚCÎT"bMÛÔ˜$Dm4°2„SÄj\Vê®nÊ°¢¤©ÎP.•²¦ÂP,EY¡)¢k8`fªÍ›æÜ¸§f&L’!VÚfk“Ô$yX?
+gpN	BY‚!J5ÿò5Ó<Õ“LÖ²¸ÂÏ¿
+ø¬BÒæ€!±ßW×å:ÍùJ25ln#pk—¸A‹éŠ3¯a¯Óeß|-Ä(7¸
+j„9ÀˆöPwOÄ8_R¯…RÛç6<ñæðøëå#Æ•oÛÏùÎ6Ý}d¬á¸a^@Öm‡ø8ÀöN…ø4àX5wžÑãt¦	 Æ:+M¢ÊÇœÈØ*êŠ÷™õ‘aqÏSö…ç¿Ø~ù¦mõWŠ¹åãIä¼I9':¢:º×ïÖU¤0“ñ,ÕÁiÎP( [`3ƒ9e:"ØA	Âó¦ë™9äôƒ…2Ì$ˆ$“T¡DçZ1°›™X•“‚Œ[rI#uwv?´WŽ‚[þÕ;ÿ=J¸Ób
+Å
+=3Û¤9ª¡N3“ÕUŠböÎ:ð€Í´Ùæ[\U£Ùöêxoß»5Dðd_|ëñ¦É[Ú‚S?¿òö…Z4¿xn>öF¸x½ïÒâ“‰g«ž\¬©xâ…òjXc088íZæ‡ý¢g­q<qCÛV$ù@šYÜòŽøó×ÙC½Ù¿FO~úä®¸:ùÑŽx/Ód:-¦˜ËÎHP·"®-j2L–£¶¼X³Ê`%#¿ †!lý?%—6Pgß‹DÖ’«wÁ#	eWµÿnôûþvU5pWµÛÿŸTéñˆQ@€vN½úá[ùéÍÏ_ú½ÿÅƒü¹ôÂíéóÞoÞ¾FýÊ	ß}U=bEOjNˆÿôøe±¢çŠ?,x^Séñ.¨aÿÊ& n+hûäbuØ¼÷Ã$£û“‹S%þDját_½?wm>?60RœÞ×ßþù¾£ÛßJ.Œ®ýÞüÌ{W{ONàœmŒŽ?ò]|&²øNéÂ_ßÑv³ôÒogmÝuâþ#ñ‰_ Æß
